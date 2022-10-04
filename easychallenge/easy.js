fed575ac392fbfa608da8c162a803fdb8d04affc
dmhebron <dmhebron21@gmail.com>
2022-10-03 23:43:32 -0400

    completed medium challenge

--- easychallenge/easy.js
+++ easychallenge/easy.js
@@ -1,10 +1,15 @@
+//used an arrow function for this challenge
+// name of the function is createExercise for both
+// message & exercise1/2 are the parameters
+
 var createExercise = (message, exercise1) => {
+    // we're going to print Today's(the value of message) + (the value of exercise1)
     return "Today's" + message + ":" + exercise1;
-    // return "Today's" + "exercise" + exercise2;
-};
+}; //we assigned the value of message= exercise and exercise1=running
 console.log(createExercise("exercise", "running"));
 
 var createExercise = (message, exercise2) => {
+     // we're going to print Today's(the value of message) + (the value of exercise2)
     return "Today's" + message + ":" + exercise2;
-};
+}; //we assigned the value of message= exercise and exercise2=swimming
 console.log(createExercise("exercise", "swimming"));
---
+++ medium challenge/medium.js
@@ -0,0 +1,26 @@
+// var sharePizza = cutPizzaSlices(8);
+// console.log(sharePizza(2));                                                                                                                                                                    
+//   // prints "Each person gets 4.00 slices of pizza"
+// console.log(sharePizza(3)); 
+//   // prints "Each person gets 2.67 slices of pizza"
+
+
+//created a function called sharePizza
+let text = "Each person gets" + sharePizza + "slices of pizza";
+var sharePizza = (cut, share) => {
+    return text + cut/share; 
+};
+console.log(sharePizza("8", "2"));
+//the cosole is going to start reading from the top
+// first it will read the string assigned to the text function
+// then it will know the parameters are cut and share
+//then it will know it will divide cut by share
+// then it will see that the value of cut is 8 and share is 2
+// finally it will put all the pieces together and print 
+// Eachpersongets4slicesofpizza
+
+let text2 = "Each person gets" + sharePizza2 + "slices of pizza";
+var sharePizza2 = (cut2, share2) => {
+    return text2 + cut2/share2; 
+};
+console.log(sharePizza2("8", "3"));
