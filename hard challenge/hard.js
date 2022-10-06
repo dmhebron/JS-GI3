4492e005dbae13295bbfb933d70e392dedafbe65
dmhebron <dmhebron21@gmail.com>
2022-10-05 22:31:40 -0400

    added comments to code

--- hard challenge/hard.js
+++ hard challenge/hard.js
@@ -1,10 +1,11 @@
-ppi = () => {
-    const person = {
+ppi = () => { //create arrow function for the ppi
+    //the () is the closure that makes a variable private
+    const person = { //create an object called person
         name: "Destiny Hebron",
         ssn: 4682389412,
     };
     return (() => person.name)();
 };
-getName = (x) => "Please deliver to" + " " + x;
-console.log(getName(ppi()));
-console.log(getName(ssn()));
+getName = (x) => "Please deliver check to" + " " + x;
+console.log(getName(ppi())); //  will return please deliver check to destiny hebron
+console.log(getName(ssn())); // will return ssn is not defined
